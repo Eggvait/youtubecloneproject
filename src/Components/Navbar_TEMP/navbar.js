3313e588b98376528a06fe6227b87b910318e18f
Advait Namjoshi <namjoshiadvait@gmail.com>
2026-01-15 01:11:58 +0530

    Left Navbar with YouTube icon and title

--- src/Components/Navbar_TEMP/navbar.js
+++ src/Components/Navbar_TEMP/navbar.js
@@ -1,10 +1,20 @@
-import React from 'react'
-import './navbar.css'
+import React from "react";
+import "./navbar.css";
+import MenuIcon from '@mui/icons-material/Menu';
+import YouTubeIcon from '@mui/icons-material/YouTube';
 
 const Navbar = () => {
-  return (
-    <div>Navbar</div>
-  )
-}
- 
-export default Navbar
+  return <div className="navbar">
+    <div className ="navbar-left">
+        <div className="navbarHamberger">
+          <MenuIcon sx ={{color: "white"}}/>
+        </div>
+        <div className="navbar_youtubeimg">
+          <YouTubeIcon sx={{fontSize: "34px" }} className="navbar_youtubeimage"/>
+          <div className="navbar_utubeTitle">YouTube</div>
+        </div>
+      </div>
+  </div>;
+};
+
+export default Navbar;
