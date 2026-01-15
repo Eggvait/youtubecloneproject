b2084cbcd56c2106ce4c44eddde9e0a647028b5b
Advait Namjoshi <namjoshiadvait@gmail.com>
2026-01-15 11:12:50 +0530

    Home Page Options

--- src/Components/HomePage/homePage.js
+++ src/Components/HomePage/homePage.js
@@ -2,9 +2,20 @@ import React from 'react'
 import './homePage.css'
 
 const HomePage = () => {
+
+  const options = ['All', 'Music', 'Sports', 'Gaming', 'News', 'Movies', 'Fashion', 'Learning', 'Live', 'Music', 'Sports', 'Gaming', 'News', 'Movies', 'Fashion', 'Learning', 'Live', 'Music', 'Sports', 'Gaming', 'News', 'Movies', 'Fashion', 'Learning', 'Live']
   return (
     <div className="homePage">
       <div className="homePage_options">
+        {
+          options.map((item,index)=>{
+            return(
+              <div key={index} className="homepage_option">
+                {item}  
+              </div>
+            );
+          })
+        }
         
       </div>
     </div>
