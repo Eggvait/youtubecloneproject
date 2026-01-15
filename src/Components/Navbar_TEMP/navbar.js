480a3dec37b35868b25df490f235ce7aecf4bb28
Advait Namjoshi <namjoshiadvait@gmail.com>
2026-01-15 08:49:45 +0530

    NAVBAR COMPLETED

--- src/Components/Navbar_TEMP/navbar.js
+++ src/Components/Navbar_TEMP/navbar.js
@@ -4,8 +4,19 @@ import MenuIcon from '@mui/icons-material/Menu';
 import YouTubeIcon from '@mui/icons-material/YouTube';
 import SearchIcon from '@mui/icons-material/Search';
 import KeyboardVoiceIcon from '@mui/icons-material/KeyboardVoice';
+import VideoCallIcon from '@mui/icons-material/VideoCall';
+import NotificationsIcon from '@mui/icons-material/Notifications';
+import PersonIcon from '@mui/icons-material/Person';
+import { useState } from "react";
 
 const Navbar = () => {
+  const[userPic, setUserPic] = useState("https://icon-library.com/images/user-icon-jpg/user-icon-jpg-29.jpg");
+  const[navbarModal, setNavbarModal] = useState(false);
+  
+  const handleClickModal = () => {
+    setNavbarModal(prev => !prev);
+  }
+
   return <div className="navbar">
     <div className ="navbar-left">
         <div className="navbarHamberger">
@@ -24,12 +35,25 @@ const Navbar = () => {
           </div>
         </div>
 
-        S
         <div className="navbar_voiceIcon">
           <KeyboardVoiceIcon sx ={{color: "white"}}/>
         </div>
     </div>
+
+    <div className="navbar-right">
+      <VideoCallIcon sx ={{color: "white", fontSize: "30px", cursor: "pointer"}}/>
+      <NotificationsIcon sx ={{color: "white", fontSize: "30px", cursor: "pointer"}}/>
+      <img onClick={handleClickModal} src={userPic} className ='navbar-right-logo' alt='logo'/>
+
+      { navbarModal && 
+      <div className= 'navbar-modal'>
+        <div className = "navbar-modal-option">Profile</div>
+        <div className = "navbar-modal-option">Logout</div>
+        <div className = "navbar-modal-option">Login</div>
+      </div>
+      }
+    </div>
   </div>;
-};
+};  
 
 export default Navbar;
