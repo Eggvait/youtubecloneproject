2cd1649df5e0c374b7f1123523233cf42c2c24b5
Advait Namjoshi <namjoshiadvait@gmail.com>
2026-01-15 08:23:52 +0530

    Middle Navbar Completed

--- src/Components/Navbar_TEMP/navbar.js
+++ src/Components/Navbar_TEMP/navbar.js
@@ -2,6 +2,8 @@ import React from "react";
 import "./navbar.css";
 import MenuIcon from '@mui/icons-material/Menu';
 import YouTubeIcon from '@mui/icons-material/YouTube';
+import SearchIcon from '@mui/icons-material/Search';
+import KeyboardVoiceIcon from '@mui/icons-material/KeyboardVoice';
 
 const Navbar = () => {
   return <div className="navbar">
@@ -13,7 +15,20 @@ const Navbar = () => {
           <YouTubeIcon sx={{fontSize: "34px" }} className="navbar_youtubeimage"/>
           <div className="navbar_utubeTitle">YouTube</div>
         </div>
-      </div>
+    </div>
+    <div className="navbar-middle">
+        <div className="navbar_searchbox">
+          <input type="text" className="navbar_searchBoxinput" placeholder="Search"/>
+          <div className="navbar_searchIcon">
+            <SearchIcon sx={{fontSize: "20px", color: "white"}} />
+          </div>
+        </div>
+
+        S
+        <div className="navbar_voiceIcon">
+          <KeyboardVoiceIcon sx ={{color: "white"}}/>
+        </div>
+    </div>
   </div>;
 };
 
